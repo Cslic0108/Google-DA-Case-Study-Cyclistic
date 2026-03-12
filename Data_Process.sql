a304c32b8d22a0e532c8285389cc370c37f55959
Cslic0108 <88975696+Cslic0108@users.noreply.github.com>
2026-03-12 15:54:55 +0800

    Update Data_Process.sql

--- Data_Process.sql
+++ Data_Process.sql
@@ -15,3 +15,11 @@ SET end_lat = NULL WHERE end_lat = '' OR end_lat = '0';
 
 UPDATE main_data 
 SET end_lng = NULL WHERE end_lng = '' OR end_lng = '0';
+
+
+-- Converting strings to `DECIMAL`
+ALTER TABLE main_data 
+MODIFY COLUMN start_lat DECIMAL(10, 8),
+MODIFY COLUMN start_lng DECIMAL(11, 8),
+MODIFY COLUMN end_lat DECIMAL(10, 8),
+MODIFY COLUMN end_lng DECIMAL(11, 8);
