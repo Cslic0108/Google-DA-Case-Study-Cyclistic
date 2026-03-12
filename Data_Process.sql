8ae175cfcd0fe111d1da3a65273c731ab94d1bc9
Cslic0108 <88975696+Cslic0108@users.noreply.github.com>
2026-03-12 15:55:13 +0800

    Update Data_Process.sql

--- Data_Process.sql
+++ Data_Process.sql
@@ -6,16 +6,12 @@ MODIFY COLUMN ended_at DATETIME;
 -- Cleanup NULL
 UPDATE main_data 
 SET start_lat = NULL WHERE start_lat = '' OR start_lat = '0';
-
 UPDATE main_data 
 SET start_lng = NULL WHERE start_lng = '' OR start_lng = '0';
-
 UPDATE main_data 
 SET end_lat = NULL WHERE end_lat = '' OR end_lat = '0';
-
 UPDATE main_data 
 SET end_lng = NULL WHERE end_lng = '' OR end_lng = '0';
-
 
 -- Converting strings to `DECIMAL`
 ALTER TABLE main_data 
